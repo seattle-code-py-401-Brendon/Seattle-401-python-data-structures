77a82639bdc8fd5afd469a946be62631a8dbaba3
Brendon <Brendonhampton1988@gmail.com>
2022-05-31 21:58:23 -0700

    got 3rd test initiated the Linked List class

--- python/tests/data_structures/test_linked_list.py
+++ python/tests/data_structures/test_linked_list.py
@@ -6,12 +6,12 @@ def test_exists():
     assert LinkedList
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_instantiate():
     assert LinkedList()
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_empty_head():
     linked = LinkedList()
     assert linked.head is None
